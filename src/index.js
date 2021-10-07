f466658fe216c2e6bc9e4329f0bd9c7e664f5c07
nguyenvu37 <nguyenanhvu.bkdn@gmail.com>
2021-10-07 19:11:19 +0700

    home page

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ ReactDOM.render(
       </React.StrictMode>
     </Provider>
   </PersistGate>,
-  document.getElementById('root'),
+  document.getElementById('container'),
 );
 
 // If you want to start measuring performance in your app, pass a function
